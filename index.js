4e7e7575afeebe0a396a524cf8e9b16bce4356eb
spa-central-styles <ui@multiplan.com>
2020-08-02 11:46:04 -0500

    1 file modified

--- index.js
+++ index.js
@@ -28,7 +28,46 @@ const vue = new Vue({
   <div class="alert-danger" style="margin:15px;">
   <b-alert show variant="danger"><i class="fa fa-fw fa-exclamation-circle"></i>This is the error alert banner</b-alert>
   </div>
-  
+  <div>
+  <b-alert show dismissible style="margin:15px;" class="alert alert-success alert-dismissible">
+    This is an success alert
+  </b-alert>
+  </div>
+  <div>
+  <b-alert show dismissible style="margin:15px;" class=" alert alert-info alert-dismissible">
+    This is an info alert
+  </b-alert>
+  </div>
+  <div>
+  <b-alert show dismissible style="margin:15px;" class=" alert alert-warning alert-dismissible">
+    This is a warning alert
+  </b-alert>
+  </div>
+  <div>
+  <b-alert show dismissible style="margin:15px;" class=" alert alert-danger alert-dismissible">
+    This is a danger alert
+  </b-alert>
+  </div>
+  <div>
+  <b-alert show dismissible style="margin:15px;" class=" alert alert-primary alert-dismissible">
+    This is a primary alert
+  </b-alert>
+  </div>
+  <div>
+  <b-alert show dismissible style="margin:15px;" class=" alert alert-secondary alert-dismissible">
+    This is a secondary alert
+  </b-alert>
+  </div>
+  <div>
+  <b-alert show dismissible style="margin:15px;" class=" alert alert-light alert-dismissible">
+    This is a light alert
+  </b-alert>
+  </div>
+  <div>
+  <b-alert show dismissible style="margin:15px;" class=" alert alert-dark alert-dismissible">
+    This is a dark alert 
+  </b-alert>
+  </div>
 
 </div>`
 });
