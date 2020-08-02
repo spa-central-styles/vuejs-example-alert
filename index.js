6f8aa40f4d426daff4c296f8394366e1b1326525
spa-central-styles <ui@multiplan.com>
2020-08-02 11:24:17 -0500

    1 file modified

--- index.js
+++ index.js
@@ -17,29 +17,18 @@ const vue = new Vue({
   template:`<div>
 
   <div class="alert-primary" style="margin:15px;">
-  <b-alert show variant="primary">  <i class="fa fa-fw fa-info-circle"></i> Primary Alert</b-alert>
-  </div>
-  <div class="alert-secondary" style="margin:15px;">
-  <b-alert show variant="secondary">Secondary Alert</b-alert>
-  </div>
-  <div class="alert-success" style="margin:15px;">
-  <b-alert show variant="success">Success Alert</b-alert>
-  </div>
-  <div class="alert-danger" style="margin:15px;">
-  <b-alert show variant="danger">Danger Alert</b-alert>
+  <b-alert show variant="primary"><i class="fa fa-fw fa-info-circle"></i> This is the informational alert banner</b-alert>
   </div>
   <div class="alert-warning" style="margin:15px;">
-  <b-alert show variant="warning">Warning Alert</b-alert>
+  <b-alert show variant="warning"><i class="fa fa-fw fa-exclamation-triangle"></i>This is the warning alert banner</b-alert>
   </div>
-  <div class="alert-info" style="margin:15px;">
-  <b-alert show variant="info">Info Alert</b-alert>
+  <div class="alert-success" style="margin:15px;">
+  <b-alert show variant="success"><i class="fa fa-fw fa-check-circle"></i>This is the success alert banner</b-alert>
   </div>
-  <div class="alert-light" style="margin:15px;">
-  <b-alert show variant="light">Light Alert</b-alert>
+  <div class="alert-danger" style="margin:15px;">
+  <b-alert show variant="danger"><i class="fa fa-fw fa-exclamation-circle"></i>This is the error alert banner</b-alert>
   </div>
-  <div class="alert-dark" style="margin:15px;">
-  <b-alert show variant="dark">Dark Alert</b-alert>
-  </div>
+  
 
 </div>`
 });
